6ff7d78dfc144d494785374f43da9709049e1f15
huynh_thien_phu <phuhuynh197@gmail.com>
2024-06-16 10:08:48 +0700

    completed function encryptPassword using hashing algorithm

--- src/PasswordGenerator.java
+++ src/PasswordGenerator.java
@@ -1,3 +1,7 @@
+import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.util.Scanner;
 
@@ -11,7 +15,8 @@ public class PasswordGenerator {
 	
 	void menu() {
 		while (true) {
-			System.out.println("1. Generate a strong password\n2. Convert your password to encrypted format");
+			System.out.println("--------------------------------");
+			System.out.println("1. Generate a strong password\n2. Convert your password to encrypted password");
 			char option = Character.toUpperCase(sc.nextLine().charAt(0));
 			
 			switch (option) {
@@ -30,18 +35,33 @@ public class PasswordGenerator {
 		}
 	}
 
+	// encrypt using hashing algorithm
 	private void encryptPassword() {
+		System.out.println("Enter your password: ");
+		String password = sc.nextLine();
 		
+		try {
+			MessageDigest md = MessageDigest.getInstance("SHA-256");
+			byte[] hash = md.digest(password.getBytes(StandardCharsets.UTF_8));
+			BigInteger number = new BigInteger(1, hash);
+			String hashedPassword = number.toString(16);
+			while(hashedPassword.length() < 32) {
+				hashedPassword = "0" +hashedPassword;
+			}
+			System.out.println("Your password after encryption is: " +hashedPassword);
+		} catch (NoSuchAlgorithmException e) {
+			System.out.println("Error occured while encrypting password");
+		}
 	}
 
 	private void generatePassword() {
-		int passwordLength = 50;
-		SecureRandom random = new SecureRandom();
 		StringBuilder password = new StringBuilder();
+		SecureRandom random = new SecureRandom();
+		int passwordLength = 30;
 		for(int i=0; i<passwordLength; i++) {
 			char randomChar = (char) (random.nextInt(95) + 32);
 			password.append(randomChar);
 		}
-		System.out.println("Your password is: " + password.toString());
+		System.out.println("Your password is:" +password.toString());
 	}
 }
